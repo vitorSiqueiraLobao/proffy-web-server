63057adbfc5bf91161f414938541ba5f8274d1db
vitorSiqueiraLobao <vitorsiqueiralobao@gmail.com>
2020-08-11 16:50:37 -0300

    Database and routes done

---
+++ src/utils/convertHourToMinutes.ts
@@ -0,0 +1,7 @@
+const convertHourToMinutes = (time : string) =>{
+    const [hour, minutes] = time.split(':').map(Number)
+    const timeInMinutes = (hour*60) + minutes
+    return timeInMinutes
+}
+
+export default convertHourToMinutes
